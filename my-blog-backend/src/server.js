e6735ef110bf261a13f1d6dfbd34cb05b2e8b83a
Kir Tro <trokir@outlook.com>
2024-06-19 09:54:37 -0400

    Add data

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,26 +1,60 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
 
-let articleInfo = [
-    {
-        name: 'learn-react',
-        upvotes: 0,
-        comments: []
-    },
-    {
-        name: 'learn-node',
-        upvotes: 0,
-        comments: []
-    },
-    {
-        name: 'learn-mongoDb',
-        upvotes: 0,
-        comments: []
-    },
-]
+
+
 const app = express();
 
 app.use(express.json())
 
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+
+    try {
+        await client.connect();
+        const db = client.db('react-blog-db');
+        const article = await db.collection('articles').findOne({ name });
+
+        if (article) {
+            res.json(article);
+        } else {
+            res.status(404).json({ error: 'Article not found' });
+        }
+    } catch (error) {
+        console.error('Error fetching article:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/api/articles', async (req, res) => {
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    try {
+        await client.connect();
+        const db = client.db('react-blog-db'); // Replace with your actual database name
+
+        const collInfos = await db.listCollections().toArray();
+        console.log('Collections in the database:');
+        collInfos.forEach((coll) => {
+            console.log(coll.name);
+        });
+    } catch (error) {
+        console.error('Error fetching collection info:', error);
+    } finally {
+        await client.close();
+    }
+    
+});
+
+app.get('/api/hello', async (req, res) => {
+   
+    res.send("collections");
+});
+
+/*
 app.put('/api/articles/:name/upvote', (req, res) => {
     const { name } = req.params;
     const article = articleInfo.find(a => a.name === name);
@@ -32,9 +66,8 @@ app.put('/api/articles/:name/upvote', (req, res) => {
         res.send('This article doesn\'t exist');
     }
 });
-
-
-
+*/
+/*
 app.post('/api/articles/:name/comments', (req, res) => {
     const { postedBy, text } = req.body;
     const { name } = req.params;
@@ -47,6 +80,8 @@ app.post('/api/articles/:name/comments', (req, res) => {
         res.send('This article doesn\'t exist');
     }
 });
+*/
+
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
