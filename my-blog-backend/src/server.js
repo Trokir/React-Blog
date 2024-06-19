131cfe0f4dafcdfdcdfdfa501c27fd66a035a1ac
Kir Tro <trokir@outlook.com>
2024-06-19 17:23:11 -0400

    Add Comment Form

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,124 +1,70 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import { db, connectToDb } from './db.js';
 
 const app = express();
-const uri = 'mongodb://127.0.0.1:27017';
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-let db;
-
 app.use(express.json());
-
-async function ConnectToDb(req, res, next) {
-    if (!db) {
-        try {
-            await client.connect();
-            db = client.db('react-blog-db');
-            console.log('Connected to the database');
-        } catch (err) {
-            console.error('Failed to connect to the database', err);
-            res.status(500).json({ error: 'Internal server error' });
-            return;
-        }
-    }
-    req.db = db;
-    next();
-}
-
-async function DisconnectFromDb(req, res, next) {
-    if (db) {
-        try {
-            await client.close();
-            db = null;
-            console.log('Disconnected from the database');
-        } catch (err) {
-            console.error('Failed to disconnect from the database', err);
-        }
-    }
-    next();
-}
-
-app.use(ConnectToDb);
 
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
+
     try {
-        const article = await req.db.collection('articles').findOne({ name });
+        const article = await db.collection('articles').findOne({ name });
+
         if (article) {
             res.json(article);
         } else {
-            res.status(404).json({ error: 'Article not found' });
+            res.sendStatus(404);
         }
     } catch (error) {
-        console.error('Error fetching article:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while fetching the article.' });
     }
-});
-
-app.get('/api/articles', async (req, res) => {
-    try {
-        const collInfos = await req.db.listCollections().toArray();
-        console.log('Collections in the database:');
-        collInfos.forEach((coll) => {
-            console.log(coll.name);
-        });
-        res.json(collInfos);
-    } catch (error) {
-        console.error('Error fetching collection info:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-app.get('/api/hello', (req, res) => {
-    res.send("Hello, world!");
 });
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
+
     try {
-        await req.db.collection('articles').updateOne({ name }, {
-            $inc: { upvotes: 1 }
-        });
-        const article = await req.db.collection('articles').findOne({ name });
+        await db.collection('articles').updateOne({ name }, { $inc: { upvotes: 1 } });
+        const article = await db.collection('articles').findOne({ name });
+
         if (article) {
-            res.send(`The ${name} now has ${article.upvotes} upvotes !!!!!`);
+            res.json(article);
         } else {
-            res.status(404).json({ error: 'Article not found' });
+            res.status(404).send("That article doesn't exist");
         }
     } catch (error) {
-        console.error('Error fetching article:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while upvoting the article.' });
     }
 });
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
     const { postedBy, text } = req.body;
+
+    if (!postedBy || !text) {
+        return res.status(400).json({ error: 'Invalid request: Both "postedBy" and "text" fields are required.' });
+    }
+
     try {
-        await req.db.collection('articles').updateOne(
-            { name },
-            {
-                $push: {
-                    comments: {
-                        postedBy,
-                        text
-                    }
-                }
-            }
-        );
-        const article = await req.db.collection('articles').findOne({ name });
+        await db.collection('articles').updateOne({ name }, { $push: { comments: { postedBy, text } } });
+        const article = await db.collection('articles').findOne({ name });
+
         if (article) {
-            res.send(article.comments);
+            res.json(article);
         } else {
-            res.status(404).json({ error: 'Article not found' });
+            res.status(404).send("That article doesn't exist!");
         }
     } catch (error) {
-        console.error('Error fetching article:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while adding a comment to the article.' });
     }
 });
 
-app.use(DisconnectFromDb);
-
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+connectToDb(() => {
+    console.log('Successfully connected to database!');
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    });
 });
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,22 +1,48 @@
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import NotFoundPage from './NotFoundPage';
+import articles from './article-content';
+import CommentsList from '../components/CommentsList';
 
-import articles from "./article-content";
-import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
-    const article = articles.find(article => article.name === articleId)
 
-    if(!article){
-     return   <NotFoundPage/>
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        loadArticleInfo();
+    }, []);
+
+    const article = articles.find(article => article.name === articleId);
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
     }
-    
+
+    if (!article) {
+        return <NotFoundPage />
+    }
+
     return (
         <>
-            <h1>{article.title}</h1>
-            {article.content.map((paragraph,i) => (
-                <p key={i}>{paragraph}</p>
-            ))}
+        <h1>{article.title}</h1>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
+        {article.content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+        ))}
+        <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
