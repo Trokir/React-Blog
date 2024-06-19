9a366d474cf37a1dafc9149c635261d50df8f7a7
Kir Tro <trokir@outlook.com>
2024-06-19 17:24:10 -0400

    aa

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 import articles from './article-content';
 import CommentsList from '../components/CommentsList';
-
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -18,7 +18,7 @@ const ArticlePage = () => {
         }
 
         loadArticleInfo();
-    }, []);
+    }, [articleId]);
 
     const article = articles.find(article => article.name === articleId);
 
@@ -34,15 +34,17 @@ const ArticlePage = () => {
 
     return (
         <>
-        <h1>{article.title}</h1>
-        <div className="upvotes-section">
-            <button onClick={addUpvote}>Upvote</button>
-            <p>This article has {articleInfo.upvotes} upvote(s)</p>
-        </div>
-        {article.content.map((paragraph, i) => (
-            <p key={i}>{paragraph}</p>
-        ))}
-        <CommentsList comments={articleInfo.comments} />
+            <h1>{article.title}</h1>
+            <div className="upvotes-section">
+                <button onClick={addUpvote}>Upvote</button>
+                <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            </div>
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+            <AddCommentForm articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
