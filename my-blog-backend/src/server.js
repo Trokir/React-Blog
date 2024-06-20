cb2ad9b9fe3dadd52e2718aa55c86e5178f40e86
Kir Tro <trokir@outlook.com>
2024-06-20 10:27:56 -0400

    Final version

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,79 +1,31 @@
 import express from 'express';
 import { db, connectToDb } from './db.js';
-import fs from 'fs'
-import admon from 'firebase-admin'
-
-
-const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
-
-admon.initializeApp({
-    credential: admon.credential.cert(credentials)
-});
 
 const app = express();
 app.use(express.json());
 
-
-
-app.use(async (req, res, next) => {
-    const { authtoken } = req.headers;
-
-    if (authtoken) {
-        try {
-            req.user = await admin.auth().verifyIdToken(authtoken);
-        } catch (e) {
-            return res.sendStatus(400);
-        }
-    }
-
-    req.user = req.user || {};
-
-    next();
-});
-
-
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
-    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        const upvoteIds = article.upvoteIds || [];
-        article.canUpvote = uid && !upvoteIds.includes(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
     }
 });
 
-app.use((req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.sendStatus(401);
-    }
-});
-
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-    const { uid } = req.user;
-
+   
+    await db.collection('articles').updateOne({ name }, {
+        $inc: { upvotes: 1 },
+    });
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        const upvoteIds = article.upvoteIds || [];
-        const canUpvote = uid && !upvoteIds.includes(uid);
-   
-        if (canUpvote) {
-            await db.collection('articles').updateOne({ name }, {
-                $inc: { upvotes: 1 },
-                $push: { upvoteIds: uid },
-            });
-        }
-
-        const updatedArticle = await db.collection('articles').findOne({ name });
-        res.json(updatedArticle);
+        res.json(article);
     } else {
         res.send('That article doesn\'t exist');
     }
@@ -81,11 +33,10 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { text } = req.body;
-    const { email } = req.user;
+    const { postedBy, text } = req.body;
 
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: { postedBy: email, text } },
+        $push: { comments: { postedBy, text } },
     });
     const article = await db.collection('articles').findOne({ name });
 
@@ -101,4 +52,4 @@ connectToDb(() => {
     app.listen(8000, () => {
         console.log('Server is listening on port 8000');
     });
-});
+})
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -8,32 +8,25 @@ import useUser from '../hooks/useUser';
 import articles from './article-content';
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [], canUpvote: false });
-    const { canUpvote } = articleInfo;
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
 
     const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const token = user && await user.getIdToken();
-            const headers = token ? { authtoken: token } : {};
-            const response = await axios.get(`/api/articles/${articleId}`, { headers });
+            const response = await axios.get(`/api/articles/${articleId}`);
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
 
-        if (!isLoading) {
-            loadArticleInfo();
-        }
-    }, [isLoading, user]);
+        loadArticleInfo();
+    }, []);
 
     const article = articles.find(article => article.name === articleId);
 
     const addUpvote = async () => {
-        const token = user && await user.getIdToken();
-        const headers = token ? { authtoken: token } : {};
-        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
@@ -44,22 +37,22 @@ const ArticlePage = () => {
 
     return (
         <>
-            <h1>{article.title}</h1>
-            <div className="upvotes-section">
-                {user
-                    ? <button onClick={addUpvote}>{canUpvote ? 'Upvote' : 'Already Upvoted'}</button>
-                    : <button>Log in to upvote</button>}
-                <p>This article has {articleInfo.upvotes} upvote(s)</p>
-            </div>
-            {article.content.map((paragraph, i) => (
-                <p key={i}>{paragraph}</p>
-            ))}
+        <h1>{article.title}</h1>
+        <div className="upvotes-section">
             {user
-                ? <AddCommentForm
-                    articleName={articleId}
-                    onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
-                : <button>Log in to add a comment</button>}
-            <CommentsList comments={articleInfo.comments} />
+                ? <button onClick={addUpvote}>Upvote</button>
+                : <button>Log in to upvote</button>}
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
+        {article.content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+        ))}
+        {user
+            ? <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            : <button>Log in to add a comment</button>}
+        <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
